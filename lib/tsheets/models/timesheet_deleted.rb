bcb9d9d6c87ae7be94a21748b2ae2bb51044ea8e
Kamil Ciemniewski <ciemniewski.kamil@gmail.com>
2015-07-07 18:03:47 +0200

    Added timesheet deleted objects

---
+++ lib/tsheets/models/timesheet_deleted.rb
@@ -0,0 +1,15 @@
+  class TSheets::Models::TimesheetDeleted < TSheets::Model
+    field :id, :integer
+    field :user_id, :integer
+    field :jobcode_id, :integer
+    field :start, :datetime
+    field :end, :datetime
+    field :date, :date
+    field :duration, :integer
+    field :locked, :integer
+    field :notes, :string
+    field :customfields, :string
+    field :created, :datetime
+    field :last_modified, :datetime
+    field :type, :string
+  end
